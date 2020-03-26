59c7cbbf4c7d62c186ece97ade83ed32ff610f4e
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-03-26 23:41:56 +0300

    Claim member adding event added

---
+++ src/Cazcez/Storage/AddMember.java
@@ -0,0 +1,17 @@
+package Cazcez.Storage;
+
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
+
+public class AddMember
+{
+    public void AddMember (Chunk chunk, Player Owner, Player member)
+    {
+
+        //TODO
+
+    }
+
+
+
+}
