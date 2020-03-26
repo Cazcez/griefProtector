3d6021d2642a04c62d79d24dfec9b59c49443054
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-03-26 23:42:19 +0300

    AntiBreak Updated

--- src/Cazcez/event.java
+++ src/Cazcez/event.java
@@ -3,11 +3,14 @@ package Cazcez;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import java.io.File;
 import java.util.Map;
 
 public class event implements Listener
@@ -26,23 +29,29 @@ public class event implements Listener
         //e.getPlayer().sendMessage("Chunk: " + e.getBlock().getChunk());
         //e.getPlayer().sendMessage("Biome: "+ e.getBlock().getBiome());
 
+        Chunk chunk = e.getBlock().getLocation().getChunk();
 
-        boolean flag = plugin.hash.containsKey(e.getBlock().getLocation().getChunk());
-        if (flag == false)
+        File claimDataFolder = new File("plugins/griefProtector/claimData", "X=" + chunk.getX() + ", Z=" + chunk.getZ()  + ".yml");
+
+        if (claimDataFolder.exists())
         {
-            return;
-        }
-        else if (plugin.hash.get(e.getBlock().getLocation().getChunk()).equals(e.getPlayer()))
-        {
-            e.getPlayer().sendMessage("ah adamım arsa seninmiş\nben de kötü adamlar arsanı sikiyo' sandım");
+            FileConfiguration dataConfiguration = YamlConfiguration.loadConfiguration(claimDataFolder);
+
+            if (dataConfiguration.get("Owner") == e.getPlayer().getUniqueId())
+            {
+                e.getPlayer().sendMessage("okey bu sensin");
+            }
+            else
+            {
+                e.getPlayer().sendMessage("hey bu arsanın bir sahibi var ve bu sen değilsin!");
+                e.setCancelled(true);
+            }
+            //TODO add check for if player is a member of claim and has permission for break
         }
         else
         {
-            e.setCancelled(true);
-            e.getPlayer().sendMessage("iptal edildi");
+            e.getPlayer().sendMessage("k kırabilirsin");
         }
-
-
 
     }
 
