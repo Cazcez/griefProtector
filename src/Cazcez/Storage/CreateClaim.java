3d6875dd8e18b4a9d2127e13abd32e44e4b4a85d
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-03-26 23:41:28 +0300

    Claim creating event added

--- src/Cazcez/Storage/CreateClaim.java
+++ src/Cazcez/Storage/CreateClaim.java
@@ -4,6 +4,7 @@ import net.minecraft.server.v1_15_R1.IPlayerFileData;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,24 +16,28 @@ import java.io.IOException;
 
 public class CreateClaim
 {
-    public File fileConfig = new File("plugins/griefProtector", "config.yml");
+    public File fileConfig = new File("plugins//griefProtector", "config.yml");
 
-    public void CreateClaimYAML(Player owner, Chunk chunk)
+    public void CreateClaimYAML(CommandSender owner, Chunk chunk)
     {
 
-        File claimDataFolder = new File("plugins" + File.separator + "griefProtector" + File.separator + "claimData" + File.separator , "X=" + chunk.getX() + ", Z=" + chunk.getZ()   + ".yml");
+        File claimDataFolder = new File("plugins/griefProtector/claimData", "X=" + chunk.getX() + ", Z=" + chunk.getZ()  + ".yml");
         FileConfiguration dataConfiguration = YamlConfiguration.loadConfiguration(claimDataFolder);
 
-        if (claimDataFolder.exists())
+
+
+        if (claimDataFolder.exists()) //check chunk is already claimed
         {
             owner.sendMessage("already claimed");
         }
         else
         {
+            if (!claimDataFolder.getParentFile().exists())
+                claimDataFolder.getParentFile().mkdirs(); // creates the /plugins/griefProtector/claimData directory if not found
+
             try
             {
-                claimDataFolder.createNewFile();
-
+                claimDataFolder.createNewFile(); // create YAML file for claim
             }
             catch (IOException e)
             {
@@ -40,9 +45,27 @@ public class CreateClaim
             }
 
         }
-        dataConfiguration.set("claimTitle", owner.getName() + "'s claim");
 
-        owner.sendMessage(String.format("" + dataConfiguration.get("claimTitle")));
+        //set YAML data
+        dataConfiguration.set("claimTitle", owner.getName() + "'s claim");
+        dataConfiguration.set("claimDescription", owner.getName() + "Default description");
+
+        dataConfiguration.set("claimProtectorCoordinate.X", 0);
+        dataConfiguration.set("claimProtectorCoordinate.Y", 60);
+        dataConfiguration.set("claimProtectorCoordinate.Z", 0);
+
+        dataConfiguration.set("claimVault", 0);
+        dataConfiguration.set("claimOwner", owner.getName());
+
+        try
+        {
+            dataConfiguration.save(claimDataFolder);
+        }
+        catch (IOException e)
+        {
+            Bukkit.getLogger().info(ChatColor.RED + "'" + chunk + ".yml' datasını kaydedemedim!");
+        }
+        owner.sendMessage((String)dataConfiguration.get("claimTitle"));
 
     }
 
